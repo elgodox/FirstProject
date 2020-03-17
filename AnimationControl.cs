1ebf7ba6dd746fde5f97df29586f876e8b8246c8
Alan <elgodox@gmail.com>
2020-03-17 17:36:14 -0300

    Nueva escena de prueba

--- AnimationControl.cs
+++ AnimationControl.cs
@@ -3,21 +3,21 @@ using System;
 
 public class AnimationControl : AnimationPlayer
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
 
-    }
+	}
 
-    public void StartExitAnimation()
-    {
-        GD.Print("Animacion de salir");
-        CurrentAnimation = "Salir";
-    }
+	public void StartExitAnimation()
+	{
+		GD.Print("Animacion de salir");
+		CurrentAnimation = "Salir";
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
