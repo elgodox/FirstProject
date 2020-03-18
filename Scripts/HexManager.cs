32ca6f9e09822c25452a0ed0fea5e68e8eb49b06
EzChe <lucassuares1997@gmail.com>
2020-03-18 10:30:39 -0300

    Mejora HexManager

--- Scripts/HexManager.cs
+++ Scripts/HexManager.cs
@@ -4,44 +4,50 @@ using System.Collections.Generic;
 
 public class HexManager : Node
 {
-	[Export] int goodOnes;
 	[Export] int badOnes;
 
-	  List <HexNode> _hexes = new List<HexNode>();
+	List <HexNode> _hexes = new List<HexNode>();
 
 	public override void _Ready()
 	{
-		var allChilds = GetChildren();
+		CheckChildsHexNodes();
+		MakeBadOnes();
+	}
 
+    private void CheckChildsHexNodes()
+    {
+        var allChilds = GetChildren();
 
 		foreach(HexNode hex in allChilds) //Todos los hijos estan obligados a ser HexNode	
 		{
 			_hexes.Add(hex);
-			GD.Print(hex.GetScript().ToString());
 		}
-
-		GD.Print("Mi cantidad de hijos es " + _hexes.Count);
-		
-		 goodOnes = _hexes.Count - badOnes;
-
-		//goodOnes = Mathf.Clamp(goodOnes, 0, _hexes.Count);
-		GD.Print("GoodOnes: " + goodOnes);
-		GD.Print("BadOnes: " + badOnes);
-
-
-		MakeBadOnes();
-		 
-	}
+    }
 
     private void MakeBadOnes()
     {
+		badOnes = Mathf.Clamp(badOnes, 0, _hexes.Count);
+
+		GD.Print("badOnes: " + badOnes);
+
+		List<int> badOnesList = new List<int>();
+		
 		for (int i = 0; i < badOnes; i++)
 		{
 			Random randomNumber = new Random();
 			int badRand = (randomNumber.Next() % _hexes.Count);
-			GD.Print("Uno de los malos va a ser el slot: " + badRand);
-			_hexes[badRand].goodOne = false;
-			_hexes[badRand].asigned = true;
+			if(!badOnesList.Contains(badRand))
+			{
+				badOnesList.Add(badRand);
+				GD.Print("Uno de los malos va a ser el slot: " + badRand);
+				_hexes[badRand].goodOne = false;
+				_hexes[badRand].asigned = true;
+			}
+			else
+			{
+				GD.Print(badRand + " salió repetido :o");
+				i --;
+			}
 		}
     }
 
