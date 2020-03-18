d85daa1eeafd9de09189c968684eb5b9889b7e6c
Alan <elgodox@gmail.com>
2020-03-18 13:40:03 -0300

    Correcion en script HexNode y cambios en LVL1

--- Scripts/HexNode.cs
+++ Scripts/HexNode.cs
@@ -3,30 +3,33 @@ using System;
 
 public class HexNode : Node
 {
-	public delegate void SendPressedNode(HexNode node);
-	public SendPressedNode nodePressed;
-	public bool goodOne = true;
-	public bool asigned;
-	AnimatedSprite sprite;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		sprite = GetChild(0) as AnimatedSprite;
-	}
+    public delegate void SendPressedNode(HexNode node);
+    public SendPressedNode nodePressed;
+    public bool goodOne = true;
+    public bool asigned;
+    AnimatedSprite sprite;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        sprite = GetChild(0) as AnimatedSprite;
+        sprite.Animation = "Idle";
+        GD.Print(sprite.Name);
+    }
 
+    void _on_button_down()
+    {
+        nodePressed(this);
+        if (goodOne)
+        {
+            GD.Print("Soy de los buenos :D");
+            sprite.Animation = "isGood";
+        }
+        else
+        {
+            GD.Print("Soy de los malos :c");
+            sprite.Animation = "isBad";
+        }
 
-	void _on_button_down()
-	{
-		nodePressed(this);
-		if(goodOne)
-		{
-			GD.Print("Soy de los buenos :D");
-			sprite.Animation = "isGood";
-		}
-		else
-			GD.Print("Soy de los malos :c");
-			sprite.Animation = "isBad";
-
-	}
+    }
 }
 
