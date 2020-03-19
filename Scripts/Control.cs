5d9bd66e95021a3ac8df31ce9d4b681b71dc2415
Alan <elgodox@gmail.com>
2020-03-19 17:34:06 -0300

    Se creo 10 niveles, El Control controla la cantidad de niveles, y se agrego animacion de glitch al aparecer los Hex

--- Scripts/Control.cs
+++ Scripts/Control.cs
@@ -5,6 +5,7 @@ public class Control : Godot.Control
 {
 
     private PackedScene hexManagerScn;
+    [Export] int level;
     public override void _Ready()
     {
        GD.Print(GetChildCount());
@@ -12,19 +13,67 @@ public class Control : Godot.Control
 
     public override void _Process(float delta)
     {
+        
+        
+        
         if(GetChildCount()==2)
         {
-            CreateHex();
+            GD.Print(GetChild(0).Name);
+            CreateHex(SceneGenerator());
+            level--;
         }
         
     }
 
-    void CreateHex()
+    void CreateHex(String path)
     {
-        hexManagerScn = ResourceLoader.Load("res://Prefabs/HexManager.tscn") as PackedScene;
+        hexManagerScn = ResourceLoader.Load(path) as PackedScene;
         HexManager e;
         e = hexManagerScn.Instance() as HexManager;
         AddChild(e);
+    }
+
+    String SceneGenerator()
+    {
+        if(level==10)
+        {
+            return "res://Prefabs/HexManager10.tscn";
+        }
+        if(level==9)
+        {
+            return "res://Prefabs/HexManager9.tscn";
+        }
+        if(level==8)
+        {
+            return "res://Prefabs/HexManager8.tscn";
+        }
+        if(level==7)
+        {
+            return "res://Prefabs/HexManager7.tscn";
+        }
+        if(level==6)
+        {
+            return "res://Prefabs/HexManager6.tscn";
+        }
+        if(level==5)
+        {
+            return "res://Prefabs/HexManager5.tscn";
+        }
+        if(level==4)
+        {
+            return "res://Prefabs/HexManager4.tscn";
+        }
+        if(level==3)
+        {
+            return "res://Prefabs/HexManager3.tscn";
+        }
+        if(level==2)
+        {
+            return "res://Prefabs/HexManager2.tscn";
+        }
+        else
+        return "res://Prefabs/HexManager.tscn";
+
     }
 
 
