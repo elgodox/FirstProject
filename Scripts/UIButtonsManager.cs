fd9e38e443889f1a494545e97513fb8a9a5436b1
EzChe <lucassuares1997@gmail.com>
2020-03-26 17:42:20 -0300

    animaciones de prefabs arregladas y Panel de ayuda

--- Scripts/UIButtonsManager.cs
+++ Scripts/UIButtonsManager.cs
@@ -3,14 +3,39 @@ using System;
 
 public class UIButtonsManager : Control
 {
-    
-    [Signal] public delegate void restartGame();
-    public override void _Ready()
+	TextureButton playButton;
+	TextureRect helpCanvas;
+	[Signal] public delegate void restartGame();
+	public override void _Ready()
+	{
+		playButton = GetNode("button_Play") as TextureButton;
+		helpCanvas = GetNode("ui_Help") as TextureRect;
+        var Mngr = Owner;
+
+        if(Mngr != null)
+        {
+            GD.Print(Mngr.Name);
+            Mngr.Connect("GameOver", this, "ActivateAgain");
+        }
+	}
+	void PlayButtonUp()
+	{
+		EmitSignal(nameof(restartGame));
+		playButton.Disabled = true;
+	}
+    void ActivateAgain()
     {
-        
+        playButton.Disabled = false;
+		GD.Print("ActivateAgain");
     }
-    void PlayButtonUp()
-    {
-        EmitSignal(nameof(restartGame));
-    }
+
+	void OnHelpButtonUp()
+	{
+		helpCanvas.Show();
+	}
+
+	void OnBackButtonUp()
+	{
+		helpCanvas.Hide();
+	}
 }
