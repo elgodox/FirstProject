5039e1a5a633a1497c7a7aa43614dbc159f62737
EzChe <lucassuares1997@gmail.com>
2020-04-01 17:28:04 -0300

    Boton de ayuda se activa de nuevo al finalizar

--- Scripts/UIButtonsManager.cs
+++ Scripts/UIButtonsManager.cs
@@ -26,7 +26,7 @@ public class UIButtonsManager : Control
 	void ActivateAgain(bool win) //La llama GameManager, señal GameOver
     {
 		ActivatePlayButton(win);
-		helpButton.Disabled = win;
+		helpButton.Disabled = !win;
 		//maxBetButton.Disabled = win;
 		//betButton.Disabled = win;
 		//collectButton.Disabled = win;
