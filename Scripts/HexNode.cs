618d6f0bed24a6caa0b7b3b1814f3b4fb545743b
EzChe <lucassuares1997@gmail.com>
2020-04-08 17:25:01 -0300

    Los nodos tienen un label

--- Scripts/HexNode.cs
+++ Scripts/HexNode.cs
@@ -9,11 +9,15 @@ public class HexNode : Node
     public bool asigned = false;
     public bool pressed = false;
     AnimatedSprite sprite;
+
+    Label myName;
     public override void _Ready()
     {
         sprite = GetChild(0) as AnimatedSprite;
         sprite.Animation = "Idle";
         sprite.Play();
+        myName = GetChild(1) as Label;
+        myName.Text = Name;
     }
     void _on_button_down()
     {
