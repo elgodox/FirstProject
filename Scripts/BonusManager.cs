65ca35cdaf7fa7075607904074bdb8d36c9fd5c0
EzChe <lucassuares1997@gmail.com>
2020-04-29 17:54:16 -0300

    Cambios para mostrar premios de bonus
    Accede a los labels con un Get
    emite la señal BonusPicked cuando recibe el nodo pickeado

--- Scripts/BonusManager.cs
+++ Scripts/BonusManager.cs
@@ -1,16 +1,25 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class BonusManager : LevelManager
 {
+	[Signal] public delegate void BonusPicked();
+
+	CurrencyLabel[] bonusPrices = new CurrencyLabel[4];
     public override void _Ready()
     {
+	    bonusPrices[0] = GetNode("Hex0/BonusPrice0") as CurrencyLabel;
+	    bonusPrices[1] = GetNode("Hex1/BonusPrice1") as CurrencyLabel;
+	    bonusPrices[2] = GetNode("Hex2/BonusPrice2") as CurrencyLabel;
+	    bonusPrices[3] = GetNode("Hex3/BonusPrice3") as CurrencyLabel;
         _nodes = new HexNode[4];
         base._Ready();
     }
     public override void ReceiveNodePressed(HexNode node)
     {
-        myGameManager.BonusFinished();
+	    EmitSignal(nameof(BonusPicked));
+        myGameManager.BonusFinished(node.bonusMultiplier);
         GD.Print("El nodo presionado tenía un multiplicador de x" + node.bonusMultiplier);
         foreach (var item in _nodes)
 		{
@@ -19,6 +28,7 @@ public class BonusManager : LevelManager
 				item.pressed = true;
 			}
 		}
+        
         animation.CurrentAnimation = "Exit";
     }
 
@@ -29,6 +39,10 @@ public class BonusManager : LevelManager
 		    var currentRandomIndex = randomPos[i];
 
 		    _nodes[i].bonusMultiplier = Constants.BONUS_MULTIPLIERS[currentRandomIndex];
+		    var price = _nodes[i].bonusMultiplier.ToString();
+		    price = price.Replace(',', '.');
+		    bonusPrices[i].UpdateMyType(price);
+		    GD.Print(bonusPrices[i].myType);
 	    }
     }
 }
