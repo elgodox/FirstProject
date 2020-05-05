5e983d81d9ade5f68b5141621332ede51db8e3e1
Alan <elgodox@gmail.com>
2020-05-05 14:10:43 -0300

    Revert "Nuevos cambios en escena de bonus y en las rutas de bonus manager hacia los botones de cada bonus"
    This reverts commit 39938580fc39ab3582bbf7cac59520c603c1255c.

--- Scripts/BonusManager.cs
+++ Scripts/BonusManager.cs
@@ -7,18 +7,13 @@ public class BonusManager : LevelManager
 {
 	[Signal] public delegate void BonusPicked();
 
-	private AnimatedSprite _initAnimation;
-	private Control _bonusCircleInteractive;
-
 	CurrencyLabel[] bonusPrices = new CurrencyLabel[4];
     public override void _Ready()
     {
-	    _initAnimation = GetNode("InitAnimation") as AnimatedSprite;
-	    _bonusCircleInteractive = GetNode("BonusCircleInteractive") as Control;
-	    bonusPrices[0] = GetNode("BonusCircleInteractive/BTN/BonusPrice") as CurrencyLabel;
-	    bonusPrices[1] = GetNode("BonusCircleInteractive/BTN2/BonusPrice") as CurrencyLabel;
-	    bonusPrices[2] = GetNode("BonusCircleInteractive/BTN3/BonusPrice") as CurrencyLabel;
-	    bonusPrices[3] = GetNode("BonusCircleInteractive/BTN4/BonusPrice") as CurrencyLabel;
+	    bonusPrices[0] = GetNode("Hex0/BonusPrice0") as CurrencyLabel;
+	    bonusPrices[1] = GetNode("Hex1/BonusPrice1") as CurrencyLabel;
+	    bonusPrices[2] = GetNode("Hex2/BonusPrice2") as CurrencyLabel;
+	    bonusPrices[3] = GetNode("Hex3/BonusPrice3") as CurrencyLabel;
         _nodes = new HexNode[4];
         base._Ready();
     }
@@ -54,11 +49,5 @@ public class BonusManager : LevelManager
 		    _nodes[i].SetMultiply("X"+bonusPrices[i].myType);
 		    GD.Print(bonusPrices[i].myType);
 	    }
-    }
-
-    public void ShowBonusCircleInteractive()
-    {
-	    _initAnimation.Hide();
-	    _bonusCircleInteractive.Show();
     }
 }
