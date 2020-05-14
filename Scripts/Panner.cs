b61d556a4202286b4e585f9974b7260b24ab40da
Alan <elgodox@gmail.com>
2020-05-14 15:45:59 -0300

    SetWhite como animacion en el ready

--- Scripts/Panner.cs
+++ Scripts/Panner.cs
@@ -4,38 +4,39 @@ using System.Collections.Generic;
 
 public class Panner : Node
 {
-    private AnimationPlayer _animation;
-    int allLevels = 10;
-    
+	private AnimationPlayer _animation;
+	int allLevels = 10;
+	
 
-    public override void _Ready()
-    {
-        _animation = GetNode("Background/AnimationPlayer") as AnimationPlayer;
-        _animation.Play();
-    }
+	public override void _Ready()
+	{
+		_animation = GetNode("Background/AnimationPlayer") as AnimationPlayer;
+		_animation.CurrentAnimation = "SetWhite";
+		_animation.Play();
+	}
 
-    public void BackToOrigin(bool win, bool bonus)
-    {
-        if (win)
-        {
-            Panning(0);
-        }
-        else
-        {
-            Panning(10);
-        }
-    }
+	public void BackToOrigin(bool win, bool bonus)
+	{
+		if (win)
+		{
+			Panning(0);
+		}
+		else
+		{
+			Panning(10);
+		}
+	}
 
-    public void BackToInitAnimation()
-    {
-        _animation.CurrentAnimation = "10";
-        _animation.Play();
-    }
-    
-    public void Panning(int currentLevel)
-    {
-        float currentAnimation = currentLevel;
-        _animation.CurrentAnimation = currentAnimation.ToString();
-        _animation.Play();
-    }
+	public void BackToInitAnimation()
+	{
+		_animation.CurrentAnimation = "10";
+		_animation.Play();
+	}
+	
+	public void Panning(int currentLevel)
+	{
+		float currentAnimation = currentLevel;
+		_animation.CurrentAnimation = currentAnimation.ToString();
+		_animation.Play();
+	}
 }
