32122e2c819d037a7e2e876f7440606c7d9a938c
EzChe <lucassuares1997@gmail.com>
2020-05-15 15:50:12 -0300

    Eliminé un print de pitch

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -106,7 +106,6 @@ public class LevelManager : Node
 		{
 			node.audio.PitchScale += .1f * (10 - myGameManager.currentLevel);
 		}
-		GD.Print(node.audio.PitchScale);
 		myGameManager.CheckHexSelected(node.goodOne, node.Name, node.bonus);
 		if (node.bonus)
 		{
